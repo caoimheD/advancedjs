0c602276967f8fee993960dc29842f89fabe0776
caoimheD <caoimhe.db5@gmail.com>
2022-09-29 17:37:33 +0000

    functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,40 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    // Code Block
+    return a + b
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    // Code Block
+    return a + b
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(4, 6);
+console.log(sum2);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('hello');
+
+const sayHello = () => console.log('hello');
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    multiple lines string
+   </p>`
+)
+console.log(returnMultipleLines());
